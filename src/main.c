d696d349b3a408b08740247638fa2e01616c0141
tanjiro <viktor64y@gmail.com>
2026-02-24 18:28:36 +0000

    feat: simple input parsing

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
@@ -17,22 +18,51 @@ int main()
     size_t len = 0;
     ssize_t chars_read;
 
-    do
-    {
-        SHELL_PROMPT
-        chars_read = getline(&input, &len, stdin);
-        if (chars_read >= 1)
-        {
-            printf("Characters read: %zd\n", chars_read);
-            printf("You entered: %s", input);
-            printf("Buffer length %lu\n", len);
-        }
-        else
-        {
-            perror("Error reading line");
-        }
+    // FIX: heap buffer overflow on input ctrl+D
 
-    } while (chars_read != -1);
+    SHELL_PROMPT
+    chars_read = getline(&input, &len, stdin);
+    if (chars_read >= 1)
+    {
+        printf("Characters read: %zd\n", chars_read);
+        printf("You entered: %s", input);
+        printf("Buffer length: %lu\n", len);
+    }
+    else
+    {
+        perror("Error reading line");
+    }
+
+    // trim newline at the end of input
+    int input_length = strlen(input);
+    if (input_length >= 1 && input[input_length] == '\0')
+    {
+        input[input_length - 1] = '\0';
+    }
+
+    // parse input as array of arguments to work with execv
+    char *argv[128] = {NULL};
+    char *token;
+    if (strstr(input, " ") != NULL)
+    {
+        token = strtok(input, " ");
+        argv[0] = token;
+        for (int i = 1; token != NULL; i++)
+        {
+            token = strtok(NULL, " ");
+            argv[i] = token;
+        }
+    }
+    else
+    {
+        argv[0] = input;
+    }
+
+    printf("Parsed input:\n");
+    for (int i = 0; argv[i] != NULL; i++)
+    {
+        printf("%s\n", argv[i]);
+    }
 
     printf("Forking\n");
     pid_t child = fork();
@@ -43,10 +73,8 @@ int main()
     }
     else if (child == 0)
     {
-        CHILD_PRINT("Running %d", getpid());
-        char *const argv[] = {"ls", "-la", NULL};
-        CHILD_PRINT("Executing ls -la");
-        int exec_status = execvp("ls", argv);
+        CHILD_PRINT("Executing ls");
+        int exec_status = execvp("ls", (char *[]){"-la", NULL});
         if (exec_status == -1)
         {
             perror("[CHILD] Error executing command ls -la, terminating");
@@ -59,13 +87,20 @@ int main()
         PARENT_PRINT("Running %d", getpid());
         PARENT_PRINT("Waiting on child %d", child);
         waitpid(child, &status, 0);
-        char *const argv[] = {"uname", "-a", NULL};
-        PARENT_PRINT("Executing uname -a");
-        int exec_status = execvp("uname", argv);
+        if (WIFSIGNALED(status))
+        {
+            perror("Error\n");
+        }
+        else if (WIFEXITED(status))
+        {
+            PARENT_PRINT("Child exited normally");
+        }
+
+        PARENT_PRINT("Executing %s", input);
+        int exec_status = execvp(argv[0], argv);
         if (exec_status == -1)
         {
-            perror("[PARENT] Error executing command uname -a, terminating");
-            exit(EXIT_FAILURE);
+            perror("[PARENT] Error executing command");
         }
     }
 
